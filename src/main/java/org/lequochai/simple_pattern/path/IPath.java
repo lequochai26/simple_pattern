b41c27179d333a2ed384c9d16b1af37bb6d3cfab
Lê Quốc Hải <lequochai26@gmail.com>
2023-10-25 01:16:13 +0700

    Added methods for IPath interface.

--- src/main/java/org/lequochai/simple_pattern/path/IPath.java
+++ src/main/java/org/lequochai/simple_pattern/path/IPath.java
@@ -1,5 +1,8 @@
 package org.lequochai.simple_pattern.path;
 
 public interface IPath {
-    
+    void add(String key, Object value);
+    void remove(String key);
+    void clear();
+    Object get(String key);
 }
